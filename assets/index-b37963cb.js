af2007d2c4c3417073de4463a860108ee2c8d9ce
Ariele Bonifacio <arielebonifacio@gmail.com>
2023-01-01 21:09:05 +0100

    Updates

--- assets/index-b37963cb.js
+++ assets/index-b37963cb.js
@@ -279,7 +279,7 @@ Error generating stack: `+o.message+`
       }
     }
   }
-`,wk="/assets/Logo-45e013d3.svg";function kk(){return Ye(gk,{children:[Q("img",{src:wk,alt:"Logo-Ignite"}),Ye("nav",{children:[Q(Kd,{to:"/",title:"Timer",children:Q(wg,{size:24})}),Q(Kd,{to:"/history",title:"History",children:Q(yg,{size:24})})]})]})}const xk=ut.div`
+`,wk="https://bonieasy.github.io/ignite-timer/assets/Logo-45e013d3.svg";function kk(){return Ye(gk,{children:[Q("img",{src:wk,alt:"Logo-Ignite"}),Ye("nav",{children:[Q(Kd,{to:"/",title:"Timer",children:Q(wg,{size:24})}),Q(Kd,{to:"/history",title:"History",children:Q(yg,{size:24})})]})]})}const xk=ut.div`
   max-width: 74rem;
   height: calc(100vh - 10rem);
   margin: 5rem auto;
